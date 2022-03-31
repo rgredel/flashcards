98ffe1917ae51c94b1545a40a59e533fd5b43c34
Radek Grędel <radoslaw.gredel@student.pk.edu.pl>
2022-03-31 22:53:02 +0200

    add and confugire connection pool

---
+++ src/main/java/pl/gredel/flashcards/db/ConnectionPool.java
@@ -0,0 +1,30 @@
+package pl.gredel.flashcards.db;
+
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ConnectionPool {
+        private static BasicDataSource ds = new BasicDataSource();
+
+        //TODO
+        //Read properties from file
+
+        static {
+            ds.setUrl("jdbc:postgresql://localhost/hplussport");
+            ds.setUsername("postgres");
+            ds.setPassword("root");
+            ds.setMinIdle(5);
+            ds.setMaxIdle(10);
+            ds.setMaxOpenPreparedStatements(100);
+        }
+
+
+        public static Connection getConnection() throws SQLException {
+                return ds.getConnection();
+        }
+
+        private ConnectionPool(){ }
+}
