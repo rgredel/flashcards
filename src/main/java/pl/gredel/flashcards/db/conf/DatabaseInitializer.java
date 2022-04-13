b6be2880605e523a02908a007d65f2a2e9a9d2cc
Radek Grędel <radoslaw.gredel@student.pk.edu.pl>
2022-04-13 15:47:12 +0200

    fix: refactor DatabaseInitializer

--- src/main/java/pl/gredel/flashcards/db/conf/DatabaseInitializer.java
+++ src/main/java/pl/gredel/flashcards/db/conf/DatabaseInitializer.java
@@ -1,7 +1,6 @@
 package pl.gredel.flashcards.db.conf;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
@@ -15,30 +14,19 @@ public class DatabaseInitializer {
     private static final Logger LOGGER = Logger.getLogger( DatabaseInitializer.class.getName() );
 
 
-    private String getStatement(){
-        BufferedReader reader = null;
+    private static String getStatement(){
         String collect;
-        try {
-            reader = new BufferedReader(new FileReader("src/main/resources/flashcard.sql"));
-
+        try (BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/flashcard.sql"))){
             collect = reader.lines()
                     .collect(Collectors.joining(System.lineSeparator()));
-        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
             LOGGER.log(Level.SEVERE, e.toString(), e);
             throw new RuntimeException(e);
         }
-        finally {
-            try {
-                reader.close();
-            } catch (IOException e) {
-                LOGGER.log(Level.SEVERE, e.toString(), e);
-            }
-        }
-
         return collect;
     }
 
-    public void init() {
+    public static void init() {
         try {
             Connection connection = ConnectionPool.getConnection();
             Statement statement = connection.createStatement();
@@ -48,5 +36,9 @@ public class DatabaseInitializer {
             throw new RuntimeException(sqlException);
         }
 
+    }
+
+    public static void main(String[] args) {
+        init();
     }
 }
